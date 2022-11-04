a2090fb004743991ce112d6994651436a28b516b
Walisson Silva <walissonsilva10@gmail.com>
2022-11-03 22:21:33 -0300

    feat: create Header component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import Logo from "../../assets/logo.svg";
 
 import * as S from "./styles";
 
 export function Home() {
   return (
     <S.Container>
-      <S.Title>Hello, World!</S.Title>
+      <S.Header>
+        <S.HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <S.TotalCars>Total de 12 carros</S.TotalCars>
+        </S.HeaderContent>
+      </S.Header>
+
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
     </S.Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,14 +1,32 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 40px;
-  font-family: ${({ theme }) => theme.fonts.primary_500};
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+`;
+
+export const HeaderContent = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 24px 24px;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
